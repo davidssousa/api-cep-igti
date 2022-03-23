3f31d1412a0a2bf89c07be7d276b10778b933be7
David Sousa <david.sousa@msn.com>
2022-03-22 23:32:06 -0300

    add vercel.json

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const porta = process.env.PORT || 8080;
+const porta = process.env.PORT || 5050;
 const app = require('./configs/express')();
 
 app.listen(porta, () => console.log(`API CEP iniciou... \nURL: http://localhost:${porta}`));
